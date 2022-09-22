f87f7f86d2fda5a14f564a3298212052ffc804c2
Your Name <you@example.com>
2022-09-22 22:00:02 +0200

    booking-log-input-data

--- psychology-website/src/components/RegContact.jsx
+++ psychology-website/src/components/RegContact.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookedMessage from "./BookedMessage";
 import "./RegContact.css";
 
 function RegContact({ clickBack }) {
   const [displayConfirm, setDisplayConfirm] = useState(true);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    phoneNum: "",
+    message: "",
+  });
+
+  useEffect(() => {
+    localStorage.setItem("form", JSON.stringify(formData));
+  }, [formData]);
+
+  const handleForm = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return { ...prevFormData, [name]: value };
+    });
+  };
 
   const bookedBtnHandle = () => {
     setDisplayConfirm(false);
+  };
+
+  const bookedFormDataHandle = (e) => {
+    console.log(formData);
   };
 
   return (
@@ -33,27 +54,44 @@ function RegContact({ clickBack }) {
 
             <input
               type="text"
+              onChange={handleForm}
+              value={formData.fullName}
+              name="fullName"
               placeholder="Enter your full name"
               className="send-email--full-name "
             />
             <input
-              type="text"
+              type="email"
+              onChange={handleForm}
+              value={formData.email}
+              name="email"
               placeholder="Enter your email adress"
               className="send-email--email-adress "
             />
             <input
-              type="text"
+              type="number"
+              onChange={handleForm}
+              value={formData.phoneNum}
+              name="phoneNum"
               placeholder="Enter your phone number"
               className="send-email--phone-number "
             />
             <textarea
+              onChange={handleForm}
+              value={formData.message}
               name="message"
               placeholder="Write your message"
               className="send-email--message"
               cols="30"
               rows="10"
             ></textarea>
-            <button onClick={bookedBtnHandle} className="send-email--btn">
+            <button
+              onClick={() => {
+                bookedBtnHandle();
+                bookedFormDataHandle();
+              }}
+              className="send-email--btn"
+            >
               Finalize Booking
             </button>
           </form>
