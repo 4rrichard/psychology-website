480d5526447f1be4db0d892e73870627fcfb76f3
Your Name <you@example.com>
2022-10-26 11:44:16 +0200

    files rearranged

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import { AuthProvider } from "./context/AuthProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <AuthProvider>
         <App />
       </AuthProvider>
