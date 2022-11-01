9dbfd1eb435b500f5eabb548dd10cb259b120e79
Your Name <you@example.com>
2022-11-01 11:30:25 +0100

    subscribe@footer responsivity fix

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -16,44 +16,46 @@ function Footer() {
     <section className="footer">
       <div className="footer--reach-me">
         <p className="footer-reach-me--text">Reach me here:</p>
-        <div
-          className="footer--social"
-          onClick={() => {
-            window.open(linkedInUrl, "_blank");
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            size="3x"
-            className="footer--social-icon"
-          />
-          <p className="footer--social--name">LinkedIn</p>
-        </div>
-        <div
-          className="footer--social"
-          onClick={() => {
-            window.open(researchGateUrl, "_blank");
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faResearchgate}
-            size="3x"
-            className="footer--social-icon"
-          />
-          <p className="footer--social--name">Research Gate</p>
-        </div>
-        <div
-          className="footer--social"
-          onClick={() => {
-            window.open(twitterUrl, "_blank");
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faTwitterSquare}
-            size="3x"
-            className="footer--social-icon"
-          />
-          <p className="footer--social--name">Twitter</p>
+        <div className="footer--social-icons">
+          <div
+            className="footer--social"
+            onClick={() => {
+              window.open(linkedInUrl, "_blank");
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="3x"
+              className="footer--social-icon"
+            />
+            <p className="footer--social--name">LinkedIn</p>
+          </div>
+          <div
+            className="footer--social"
+            onClick={() => {
+              window.open(researchGateUrl, "_blank");
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faResearchgate}
+              size="3x"
+              className="footer--social-icon"
+            />
+            <p className="footer--social--name">Research Gate</p>
+          </div>
+          <div
+            className="footer--social"
+            onClick={() => {
+              window.open(twitterUrl, "_blank");
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faTwitterSquare}
+              size="3x"
+              className="footer--social-icon"
+            />
+            <p className="footer--social--name">Twitter</p>
+          </div>
         </div>
       </div>
       <div className="footer--creator">
