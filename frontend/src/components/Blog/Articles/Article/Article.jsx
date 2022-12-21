b1f304971fec1a2f1e22ea55eaaa50d8eb324037
Your Name <you@example.com>
2022-12-21 14:16:55 +0100

    article data import

--- frontend/src/components/Blog/Articles/Article/Article.jsx
+++ frontend/src/components/Blog/Articles/Article/Article.jsx
@@ -5,9 +5,14 @@ import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 import parse from "html-react-parser";
 
 import axios from "../../../../api/axios";
+import "./Article.css";
 
 function Article() {
-  const [delta, setDelta] = useState(null);
+  const [delta, setDelta] = useState([]);
+
+  console.log(delta);
+
+  const [full, setFull] = useState({});
 
   const { pageName } = useParams();
 
@@ -17,16 +22,19 @@ function Article() {
         (element) =>
           element.title.toLowerCase().split(" ").join("-") === pageName
       );
+
+      setFull(filtered);
       setDelta(filtered.textBody.ops);
     });
   }, [pageName]);
 
   return (
-    <div>
-      <div className="all-delta-container">
-        <div className="converted-view">
-          {parse(new QuillDeltaToHtmlConverter(delta).convert())}
-        </div>
+    <div className="post">
+      <h1 className="title--post">{full.title}</h1>
+      <h3 className="desc--post">{full.desc}</h3>
+      <img src={full.photo} alt="img" className="img--post" />
+      <div className="body--post">
+        {parse(new QuillDeltaToHtmlConverter(delta).convert())}
       </div>
     </div>
   );
