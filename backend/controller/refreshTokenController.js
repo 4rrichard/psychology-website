a4036416fb0605dfd3f86b07c9173c9f96ecb8d3
4rrichard <102076693+4rrichard@users.noreply.github.com>
2025-11-04 15:43:54 +0100

    fix: remove incorrect token-secret comparison from refreshTokenController

--- backend/controller/refreshTokenController.js
+++ backend/controller/refreshTokenController.js
@@ -2,31 +2,32 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const handleRefreshToken = (req, res) => {
-  const {
-    REACT_APP_USERNAME,
-    REACT_APP_ACCESS_TOKEN_SECRET,
-    REACT_APP_REFRESH_TOKEN_SECRET,
-  } = process.env;
-  const cookies = req.cookies;
-  console.log(cookies);
-  if (!cookies.jwt) return res.status(401);
-  console.log(cookies.jwt);
-  const refreshToken = cookies.jwt;
+    const {
+        REACT_APP_USERNAME,
+        REACT_APP_ACCESS_TOKEN_SECRET,
+        REACT_APP_REFRESH_TOKEN_SECRET,
+    } = process.env;
+    const cookies = req.cookies;
+    console.log(cookies);
 
-  if (REACT_APP_REFRESH_TOKEN_SECRET !== refreshToken)
-    return res.sendStatus(403);
+    if (!cookies.jwt) return res.status(401);
 
-  jwt.verify(refreshToken, REACT_APP_REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err || REACT_APP_USERNAME !== decoded.user) return res.sendStatus(403);
-    const accessToken = jwt.sign(
-      {
-        user: decoded.user,
-      },
-      REACT_APP_ACCESS_TOKEN_SECRET,
-      { expiresIn: "15m" }
-    );
-    res.json({ accessToken });
-  });
+    const refreshToken = cookies.jwt;
+
+    jwt.verify(refreshToken, REACT_APP_REFRESH_TOKEN_SECRET, (err, decoded) => {
+        if (err || REACT_APP_USERNAME !== decoded.user)
+            return res.sendStatus(403);
+
+        const accessToken = jwt.sign(
+            {
+                user: decoded.user,
+            },
+            REACT_APP_ACCESS_TOKEN_SECRET,
+            { expiresIn: "15m" }
+        );
+
+        res.json({ accessToken });
+    });
 };
 
 module.exports = { handleRefreshToken };
